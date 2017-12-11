2b79b3d3b8802d1a52f5bec9e3b208f4da958b13
Oleksandr Gunkov <oleksandr.gunkov@dish.com>
2017-12-11 17:19:49 +0200

    Overseer idea

---
+++ src/main/scala/test/throttly/Overseer.scala
@@ -0,0 +1,14 @@
+package test.throttly
+
+import akka.actor.Actor
+import akka.actor.Actor.Receive
+
+
+// wrapping internal requests
+class Overseer extends Actor {
+  // store info
+
+  override def receive = {
+
+  }
+}
